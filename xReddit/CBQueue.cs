0c95cf8466bb7af28d6d0669a6da20fe4f49b861
Michael Schwarz <michael@xware-cg.com>
2013-07-22 16:02:42 +0200

    + Own Queue
    Signed-off-by: Michael Schwarz <michael@xware-cg.com>

---
+++ xReddit/CBQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xReddit
+{
+    public class CBQueue
+    {
+        private List<RAPIQO> _list = new List<RAPIQO>();
+
+        public CBQueue ()
+        {
+        }
+
+        public void Enqueue ( RAPIQO item )
+        {
+            this.Enqueue(item, false);
+        }
+
+        public void Enqueue ( RAPIQO item, bool top )
+        {
+            if ( top )
+            {
+                this._list.Insert(0, item);
+            }
+            else
+            {
+                this._list.Add(item);
+            }
+        }
+
+        public RAPIQO Dequeue ()
+        {
+            if ( this._list.Count > 0 )
+            {
+                RAPIQO retitem = this._list[0];
+                this._list.RemoveAt(0);
+                return retitem;
+            }
+
+            return null;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this._list.Count;
+            }
+        }
+    }
+}
